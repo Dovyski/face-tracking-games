e753da13ed7c208faaf0383fb1a0a7db60384f6a
Fernando Bevilacqua <dovyski@gmail.com>
2015-10-26 09:16:03 +0100

    Structure for questions.

--- card-flipper/card-flipper.js
+++ card-flipper/card-flipper.js
@@ -2,10 +2,13 @@
  * This is the initial point for the game.
  */
 
-var game = new Phaser.Game(Constants.GAME_WIDTH, Constants.GAME_HEIGHT, Phaser.AUTO, 'card-flipper', { preload: preload, create: create });
+var game = new Phaser.Game(Constants.GAME_WIDTH, Constants.GAME_HEIGHT, Phaser.AUTO, 'card-flipper', { preload: preload, create: create, update: update });
 
 
-var mCards; // group with all the cards
+var mCards; 			// group with all the cards
+var mFlipTimer;			// interval, in seconds, between card flips
+var mQuestionTimer;		// interval, in seconds, between questions
+var mQuestion;			// Info about the current question
 
 function create() {
 	var i,
@@ -22,6 +25,43 @@ function create() {
 		aCard = new Card(game, game.world.width * 0.15 + 160 * (i % Constants.CARDS_PER_ROW), game.world.height * 0.15 + 230 * j);
 		mCards.add(aCard);
 	}
+
+	mFlipTimer = 0;
+	mQuestionTimer = 0;
+
+	// Define the current question. 'color' refer to the
+	// expected color in the answer card, 'odd' refers if
+	// the card number should be odd/even.
+	mQuestion = {
+		color: 1,
+		odd: true
+	};
+}
+
+function update () {
+	// Update counters regarding quesitons and card flips
+	mFlipTimer -= game.time.elapsed;
+	mQuestionTimer -= game.time.elapsed;
+
+	// Check if it is time to flip a new card
+	if(mFlipTimer <= 0) {
+		flipRandomCard();
+		mFlipTimer = game.rnd.realInRange(Constants.QUESTION_MIN_FLIP_CARD, Constants.QUESTION_MAX_FLIP_CARD);
+	}
+
+	// Check if it is time to ask a new question
+	if(mQuestionTimer <= 0) {
+		generateNewQuestion();
+		mQuestionTimer = game.rnd.realInRange(Constants.QUESTION_MIN_INTERVAL, Constants.QUESTION_MAX_INTERVAL);
+	}
+}
+
+function flipRandomCard() {
+	console.log('Flip card!');
+}
+
+function generateNewQuestion() {
+	console.log('Change question');
 }
 
 function preload () {
--- card-flipper/constants.js
+++ card-flipper/constants.js
@@ -6,8 +6,15 @@ var Constants = new function() {
 	this.GAME_WIDTH 			= 1024;
 	this.GAME_HEIGHT 			= 768;
 
-	this.CARDS_MAX 				= 12;	// Total number of cards in the game
-	this.CARDS_PER_ROW 			= 4;	// Amount of cards in each row
-	this.CARDS_MAX_NUMBER		= 15;	// Amount of cards in each row
-	this.CARDS_MAX_COLORS		= 3;	// Amount of cards in each row
+	this.CARDS_MAX 				= 12;		// Total number of cards in the game
+	this.CARDS_PER_ROW 			= 4;		// Amount of cards in each row
+	this.CARDS_MAX_NUMBER		= 15;		// Amount of cards in each row
+	this.CARDS_MAX_COLORS		= 3;		// Amount of cards in each row
+	this.CARDS_MIN_FLIP_SHOW	= 1000;		// Min time, in milliseconds, a card show be shown to the player
+	this.CARDS_MAX_FLIP_SHOW	= 3000;		// Max time, in milliseconds, a card show be shown to the player
+
+	this.QUESTION_MIN_INTERVAL	= 3000;		// Min time, in milliseconds, a question should remain active
+	this.QUESTION_MAX_INTERVAL	= 10000;	// Max time, in milliseconds, a question should remain active
+	this.QUESTION_MIN_FLIP_CARD	= 2000;		// Min time, in milliseconds, the game should wait until it flips a new card
+	this.QUESTION_MAX_FLIP_CARD	= 4000;		// Max time, in milliseconds, the game should wait until it flips a new card
 };
