f651790e802c87786adc6e818aa7209fcbff05d8
Fernando Bevilacqua <dovyski@gmail.com>
2015-10-29 11:21:37 +0100

    Use sqlite to store collected data.

--- backend/index.php
+++ backend/index.php
@@ -5,13 +5,28 @@
  * in a database
  */
 
+define('DB_FILE','database.sqlite');
+define('DB_FILE_PATH', dirname(__FILE__) . '/' . DB_FILE);
+
 $aUid 	= isset($_REQUEST['uid']) ? $_REQUEST['uid'] : '';
 $aData 	= isset($_REQUEST['data']) ? $_REQUEST['data'] : '';
 
 $aFile	= time() . '-' . $aUid;
 
 if($aUid != '') {
-	file_put_contents($aFile, $aUid . "\n" . $aData);
+	$aCreateTables = !file_exists(DB_FILE);
+	$aDb = new PDO('sqlite:' . DB_FILE);
+
+	if($aCreateTables) {
+		$aDb->query('CREATE TABLE logs (uuid VARCHAR(36), data TEXT)');
+	}
+
+	$aStmt = $aDb->prepare("INSERT INTO logs (uuid, data) VALUES (:uuid, :data)");
+
+	$aStmt->bindParam(':uuid', $aUid);
+	$aStmt->bindParam(':data', $aData);
+	$aStmt->execute();
+
 } else {
 	echo 'Empty UID.';
 }
