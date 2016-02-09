82991dd6496caa54bd5b1bae17b77df9d53e16dc
Fernando Bevilacqua <dovyski@gmail.com>
2016-02-09 16:22:25 +0100

    Update questions

--- js/ftg.questionnaire.js
+++ js/ftg.questionnaire.js
@@ -12,25 +12,55 @@
      this.mCallbackContext = theCallbackContext || this;
      this.mQuestions = [
          {
-             text: 'my question?1',
+             text: 'On a scale from 1 to 5, how <em>bored</em> did you feel at the <em>beginning</em> of the game?',
              options: [
-                 {value: 1, label: 'Not bored'},
-                 {value: 2, label: 'Bored2'},
-                 {value: 3, label: 'Neutral'},
-                 {value: 4, label: 'Bored4'},
-                 {value: 5, label: 'Bored5'},
+                 {value: 1, label: '(Not bored at all)'},
+                 {value: 2, label: ''},
+                 {value: 3, label: ''},
+                 {value: 4, label: ''},
+                 {value: 5, label: '(Extremely bored)'}
              ]
          },
          {
-             text: 'my question?2',
+             text: 'On a scale from 1 to 5, how <em>stressed</em> did you feel at the <em>beginning</em> of the game?',
              options: [
-                 {value: 1, label: 'Not bored'},
-                 {value: 2, label: 'Bored2'},
-                 {value: 3, label: 'Neutral'},
-                 {value: 4, label: 'Bored4'},
-                 {value: 5, label: 'Bored5'},
+                 {value: 1, label: '(Not stressed at all)'},
+                 {value: 2, label: ''},
+                 {value: 3, label: ''},
+                 {value: 4, label: ''},
+                 {value: 5, label: '(Extremely stressed)'}
              ]
-         }
+         },
+         {
+             text: 'On a scale from 1 to 5, how <em>bored</em> did you feel at the <em>end</em> of the game?',
+             options: [
+                 {value: 1, label: '(Not bored at all)'},
+                 {value: 2, label: ''},
+                 {value: 3, label: ''},
+                 {value: 4, label: ''},
+                 {value: 5, label: '(Extremely bored)'}
+             ]
+         },
+         {
+             text: 'On a scale from 1 to 5, how <em>stressed</em> did you feel at the <em>end</em> of the game?',
+             options: [
+                 {value: 1, label: '(Not stressed at all)'},
+                 {value: 2, label: ''},
+                 {value: 3, label: ''},
+                 {value: 4, label: ''},
+                 {value: 5, label: '(Extremely stressed)'}
+             ]
+         },
+         {
+             text: 'On a scale from 1 to 5 (1: initial moments of the game, 5: final moments of the game), which part would <em>best</em> describe the moment you enjoyed the most (e.g. had more fun)?',
+             options: [
+                 {value: 1, label: ''},
+                 {value: 2, label: ''},
+                 {value: 3, label: ''},
+                 {value: 4, label: ''},
+                 {value: 5, label: ''}
+             ]
+         },
      ];
 
      this.init();
