50a985d01c51898675006b52ca29ad796d997595
Fernando Bevilacqua <dovyski@gmail.com>
2016-02-10 15:29:44 +0100

    More balancing

--- platformer/Constants.js
+++ platformer/Constants.js
@@ -7,18 +7,19 @@ var Constants = new function() {
 	this.GAME_SEED 				= 1234;
 	this.GAME_WIDTH 			= 1024;
 	this.GAME_HEIGHT 			= 768;
-	this.GAME_MATCH_DURATION	= 5 * 60 * 1000; // Duration, in milliseconds, of a single match.
+	this.GAME_MATCH_DURATION	= 9 * 60 * 1000; // Duration, in milliseconds, of a single match.
 	this.GAME_ENABLE_DATA_LOG	= true; 	// Defines if the game should collect and send anonymous data regarding usage
 	this.GAME_HEALTH_MAX		= 100; 		// Max health points available in the game.
 
 	this.PLAYER_POSITION_X		= 0.15; 	// Relative to screen width, percentage of player's position (from the left).
 
 	this.DIFFICULTY	= [						// Controls the difficulty of the game. Each entry means a piece of time
-		{
+		{ // 0
 			speed: -150,					// Speed of the platforms
-			collectable_chance: 0.7,		// Chances of adding collectables
-			collectables_per_platforms: 2,	// Number of collectables for each platform
+			collectable_chance: 0.5,		// Chances of adding collectables
+			collectables_per_platforms: 1,	// Number of collectables for each platform
 			collectable_spacing: 300,		// Amount of pixels between the collectables.
+			collectable_max_height: 0,		// Max height of a high collectable.
 			obstacles_chance: 0.3,			// Chances of adding obstacles on a platform
 			obstacles_per_platform: 1,		// Number of obstacles for each platform
 			obstacle_min_pos: 250,			// Min. amount of pixels from the begining of the platform until the obstacle.
@@ -29,11 +30,12 @@ var Constants = new function() {
 			hurt: 4,						// How many health points the player loses when hit by an obstacle.
 			heal: 2							// How many health points the player gets when a heart is collected.
 		},
-		{
+		{ // 1
 			speed: -175,
-			collectable_chance: 0.8,
-			collectables_per_platforms: 2,
+			collectable_chance: 0.7,
+			collectables_per_platforms: 1,
 			collectable_spacing: 300,
+			collectable_max_height: 0,
 			obstacles_chance: 0.6,
 			obstacles_per_platform: 1,
 			obstacle_min_pos: 200,
@@ -42,13 +44,14 @@ var Constants = new function() {
 			slope_max_hight: 0.4,
 			slope_min_hight: 0.65,
 			hurt: 4,
-			heart: 2
+			heal: 2
 		},
-		{
+		{ // 2
 			speed: -200,
 			collectable_chance: 0.9,
 			collectables_per_platforms: 2,
 			collectable_spacing: 300,
+			collectable_max_height: 150,
 			obstacles_chance: 0.85,
 			obstacles_per_platform: 1,
 			obstacle_min_pos: 180,
@@ -57,13 +60,14 @@ var Constants = new function() {
 			slope_max_hight: 0.4,
 			slope_min_hight: 0.7,
 			hurt: 4,
-			heart: 2
+			heal: 2
 		},
-		{
+		{ // 3
 			speed: -225,
 			collectable_chance: 1,
 			collectables_per_platforms: 2,
 			collectable_spacing: 300,
+			collectable_max_height: 150,
 			obstacles_chance: 0.9,
 			obstacles_per_platform: 2,
 			obstacle_min_pos: 170,
@@ -72,13 +76,14 @@ var Constants = new function() {
 			slope_max_hight: 0.4,
 			slope_min_hight: 0.75,
 			hurt: 4,
-			heart: 2
+			heal: 2
 		},
-		{
+		{ // 4
 			speed: -250,
-			collectable_chance: 1,
-			collectables_per_platforms: 2,
+			collectable_chance: 0.7,
+			collectables_per_platforms: 1,
 			collectable_spacing: 300,
+			collectable_max_height: 150,
 			obstacles_chance: 1,
 			obstacles_per_platform: 2,
 			obstacle_min_pos: 130,
@@ -86,8 +91,72 @@ var Constants = new function() {
 			platforms_before_slope: 1,
 			slope_max_hight: 0.35,
 			slope_min_hight: 0.8,
-			hurt: 4,
-			heart: 2
+			hurt: 6,
+			heal: 1
+		},
+		{ // 5
+			speed: -300,
+			collectable_chance: 0.5,
+			collectables_per_platforms: 1,
+			collectable_spacing: 300,
+			collectable_max_height: 150,
+			obstacles_chance: 1,
+			obstacles_per_platform: 2,
+			obstacle_min_pos: 130,
+			obstacle_spacing: 300,
+			platforms_before_slope: 1,
+			slope_max_hight: 0.35,
+			slope_min_hight: 0.8,
+			hurt: 8,
+			heal: 1
+		},
+		{ // 6
+			speed: -350,
+			collectable_chance: 0.4,
+			collectables_per_platforms: 1,
+			collectable_spacing: 300,
+			collectable_max_height: 150,
+			obstacles_chance: 1,
+			obstacles_per_platform: 2,
+			obstacle_min_pos: 130,
+			obstacle_spacing: 300,
+			platforms_before_slope: 1,
+			slope_max_hight: 0.35,
+			slope_min_hight: 0.8,
+			hurt: 10,
+			heal: 1
+		},
+		{ // 7
+			speed: -450,
+			collectable_chance: 0.2,
+			collectables_per_platforms: 1,
+			collectable_spacing: 300,
+			collectable_max_height: 150,
+			obstacles_chance: 0.9,
+			obstacles_per_platform: 3,
+			obstacle_min_pos: 50,
+			obstacle_spacing: 220,
+			platforms_before_slope: 1,
+			slope_max_hight: 0.35,
+			slope_min_hight: 0.9,
+			hurt: 10,
+			heal: 1
+		},
+		{ // 8
+			speed: -550,
+			collectable_chance: 0.1,
+			collectables_per_platforms: 1,
+			collectable_spacing: 300,
+			collectable_max_height: 150,
+			obstacles_chance: 0.9,
+			obstacles_per_platform: 3,
+			obstacle_min_pos: 50,
+			obstacle_spacing: 220,
+			platforms_before_slope: 2,
+			slope_max_hight: 0.35,
+			slope_min_hight: 0.9,
+			hurt: 15,
+			heal: 1
 		}
 	];
 };
