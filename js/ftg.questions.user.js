51254b8e5c235bcb0d29fdede0dbdb752f209a00
Fernando Bevilacqua <dovyski@gmail.com>
2016-02-15 10:21:13 +0100

    Update user questionnaire

--- js/ftg.questions.user.js
+++ js/ftg.questions.user.js
@@ -29,8 +29,27 @@
          options: [
              {value: 1, label: 'Very skilled'},
              {value: 2, label: 'Moderately skilled'},
-             {value: 3, label: 'Not very skilled1 to 3'},
+             {value: 3, label: 'Not very skilled'},
              {value: 4, label: 'No skill'}
+         ]
+     },
+     {
+         text: 'Compared to your regular/usual daily state of mind, how are you feeling today?',
+         hide: true,
+         options: [
+             {value: 1, label: 'Unusually calm and relaxed'},
+             {value: 2, label: 'A bit more calm and relaxed than usual'},
+             {value: 3, label: 'Normal (calm and/or stressed as usual'},
+             {value: 4, label: 'A bit more stressed than usual'},
+             {value: 4, label: 'Unusually stressed'}
+         ]
+     },
+     {
+         text: 'Which of the following profiles describes you better in terms of what type of digital game player you are?',
+         hide: true,
+         options: [
+             {value: 1, label: 'I don’t like challenges, I just want easy games (they are fun)'},
+             {value: 2, label: 'I like challenging games, the more difficult, the better'}
          ]
      },
  ];
