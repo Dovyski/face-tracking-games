b406b80182b0fc2cf42faed84452b103793c8d65
Fernando Bevilacqua <dovyski@gmail.com>
2016-02-17 11:44:07 +0100

    Scale loading state to fill the screen

--- platformer/BootState.js
+++ platformer/BootState.js
@@ -11,6 +11,9 @@ var BootState = function() {
 		// Set the mood
 		Game.stage.backgroundColor = 0xFFCC99;
 
+		// Scale the game to fill the whole screen, keeping the aspect ratio.
+		this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
 		Game.load.image('loading-background', 'assets/loading_background.png'); // UI pack, by Kenney Vleugels (www.kenney.nl), CC0, http://www.kenney.nl
 		Game.load.image('loading-fill', 'assets/loading_fill.png'); // UI pack, by Kenney Vleugels (www.kenney.nl), CC0, http://www.kenney.nl
 	};
